719c0e49b0a0116459242239769929f178b6c2ed
tmkeating <tmkeating@users.noreply.github.com>
2026-02-10 16:32:02 +0100

    Update SLURM script to match DCC Cluster Guide (mhigh,mlow + masterlow QOS)

--- run_h_pylori.sh
+++ run_h_pylori.sh
@@ -2,12 +2,15 @@
 #SBATCH --job-name=h_pylori_train       # Name of the job
 #SBATCH --output=results/output_%j.txt # Standard output and error log (%j = JobID)
 #SBATCH --error=results/error_%j.txt
-#SBATCH --ntasks=1                     # Run a single task
-#SBATCH --cpus-per-task=4              # Number of CPU cores per task
+#SBATCH -n 4                           # Number of cores
+#SBATCH -N 1                           # Ensure that all cores are on one machine
+#SBATCH -t 0-12:00                     # Runtime in D-HH:MM (12 hours)
+#SBATCH -p mhigh,mlow                  # Partition to submit to (Master High/Low)
+#SBATCH -q masterlow                   # Quality of Service (QOS)
 #SBATCH --mem=32G                      # Total memory
 #SBATCH --gres=gpu:1                   # Request 1 GPU
-#SBATCH --time=12:00:00                # Time limit hrs:min:sec
-# #SBATCH --partition=...              # Commented out to use default partition
+#SBATCH -o results/%x_%j.out           # Standard output log
+#SBATCH -e results/%x_%j.err           # Standard error log
 
 # 1. Load necessary modules (Uncomment and adjust based on your cluster)
 # module load python/3.8
