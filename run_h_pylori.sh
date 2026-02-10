272c316f4392397afd7f6fa6acc79335910220ba
tmkeating <tmkeating@users.noreply.github.com>
2026-02-10 16:49:59 +0100

    Changed memory allocation from 32G to 8G in the SLURM job script, and resized input images to (256, 256) in the dataset transformation.

--- run_h_pylori.sh
+++ run_h_pylori.sh
@@ -5,7 +5,7 @@
 #SBATCH -N 1                           # Ensure that all cores are on one machine
 #SBATCH -t 0-12:00                     # Runtime in D-HH:MM (12 hours)
 #SBATCH -p dcca40                      # Submit to the A40 node partition
-#SBATCH --mem=32G                      # Total memory
+#SBATCH --mem=8G                      # Total memory
 #SBATCH --gres=gpu:1                   # Request 1 GPU
 #SBATCH -o results/output_%j.txt       # Standard output log (JobID %j)
 #SBATCH -e results/error_%j.txt        # Error log
